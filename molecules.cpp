598d3138b9fab86e2680a6187245c5fc3db8f6bd
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-11-09 21:32:43 +0600

    11/09/23

---
+++ molecules.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long ;
+
+void solve(void)
+{
+    int d,a,b,c;
+    cin>>a>>b>>c;
+
+    ll ma=max(max(a,b),c);
+
+    d=(a+b+c)-ma;
+
+    if(d<ma)
+    cout<<"Impossible"<<'\n';
+    
+    if((a+b+c)%2==0)
+    {
+        ll x,y,z;
+        x=(a+b-c)/2;
+        y=(b+c-a)/2;
+        z=(c+a-b)/2;
+        cout<<x<<" "<<y<<" "<<z<<'\n';
+    }
+    else
+    cout<<"Impossible"<<'\n';
+}
+
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t;
+  t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
