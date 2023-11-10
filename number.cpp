fa74d3ae00606a620c4e40091b8658db069afa01
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-11-11 01:59:12 +0600

    11/11/23

---
+++ number.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<tuple>
+#include<utility>
+#include<algorithm>
+#define vi vector<int>v
+#define pi pair<int,int>
+using namespace std;
+using ll=long long ;
+
+void solve(void)
+{
+    int n,a,b;
+    cin>>n>>a>>b;
+    int r;
+    r=max(a+1,n-b);
+
+    cout<<(n-r+1)<<'\n';
+    
+
+}
+
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t;
+  t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
