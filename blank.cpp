5d154f34e3b9b467c1152423305d3dfebd81b741
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-11-15 08:00:09 +0600

    11/15/23

---
+++ blank.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<tuple>
+#include<utility>
+#include<algorithm>
+#define vi vector<int>v
+#define pi pair<int,int>
+using namespace std;
+using ll=long long ;
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int cnt = 0, ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if(a[i] == 0)
+        {
+            cnt++;
+        }
+        else
+        {
+            ans = max(ans, cnt);
+            cnt = 0;
+        }
+    }
+    cout << max(ans, cnt) << endl;
+}
+
+
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
