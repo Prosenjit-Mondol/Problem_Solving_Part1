1882acd8469a9b9341c7063daef69a689f1dfe1e
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-11-28 01:02:34 +0600

    11/28/23

--- acquarium.cpp
+++ acquarium.cpp
@@ -2,61 +2,44 @@
 #include<tuple>
 #include<utility>
 #include<algorithm>
-#define vi vector<int>v
-#define pi pair<int,int>
 using namespace std;
-using ll=long long ;
+const int inf = 3e5;
+using ll=unsigned long long ;
+
 
 void solve(void)
 {
-    ll n,x;
+    ll n,x,cover=0,total;
     cin>>n>>x;
-    vector<int>v;
+    vector<ll>v;
     for(int i=0;i<n;i++)
     {
     int x;
     cin>>x;
     v.push_back(x);
     }
-    sort(v.begin(),v.end());
-    ll sum=0,z,r=x/n+v[n-1];
-    if(x==1)
+    ll l=0,r=1e11,re=0;
+    while (l<=r)
     {
-        if((v[1]-v[0])>0)
-        cout<<(v[0]+1)<<'\n';
+        ll mid=(l+r)/2;
+        total=mid*n;
+        cover=0;
+        for (int i = 0; i <n; i++)
+        {
+            cover+=min(mid,v[i]);
+        }
+        ll space=total-cover;
+        if(x>=space)
+        {
+            l=mid+1;
+            re=max(re,mid);
+        }
         else
-        cout<<v[0]<<'\n';
-        return;
+        {
+            r=mid-1;
+        }
     }
-    z=r;
-    for (ll i = r; i >=v[0]; i--)
-    {
-        sum=0;
-        for (ll j=0;j<n;j++)
-        {
-            sum+=(i-v[j]);
-            
-        }
-        //cout<<r<<sum<<" ";
-        if (sum==x)
-        {
-            cout<<i<<'\n';
-            return;
-        }
-        else if(sum<x)
-        {
-            break;
-        }
-        else if(sum>x)
-        {
-            z=i;
-        }
-        
-    }
-    if(z==r)
-    cout<<z<<'\n';
-    else
-    cout<<(z-1)<<'\n';
+    cout<<re<<'\n';
 }
 
 
@@ -65,7 +48,7 @@ int main()
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-  ll t;
+  ll t=1;
   cin>>t;
     while(t--)
      {
