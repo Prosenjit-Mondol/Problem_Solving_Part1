937731b82ebfbe48fbae89029b2264c78fc097ae
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-12-12 00:57:04 +0600

    12/12/23

--- zzz.cpp
+++ zzz.cpp
@@ -1,4 +1,3 @@
-
 #include<bits/stdc++.h>
 #include<tuple>
 #include<utility>
@@ -6,15 +5,44 @@
 using namespace std;
 const int inf = 3e5;
 using ll=unsigned long long ;
-
+ 
+ int coun(string a,string b){
+    int val=0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        val+=abs(a[i]-b[i]);
+    }
+    return val;
+ }
 void solve(void)
 {
-    double num=61803398874989484820458683436563811772030917980576;
-    //cin>>num;
-    ostringstream str1;
-    str1 << num;
-    string geek = str1.str();
-    cout<<geek;
+    int n,m;
+    cin>>n>>m;
+    int a[n+5][m+5];
+    for (int i = 0; i <n; i++)
+    {
+      for (int j= 0; j< m; j++)
+      {
+        cin>>a[i][j];
+      }
+    }
+    if (n==2)
+    {
+      cout<<1<<'\n';
+    }
+    else if (n==4)
+    {
+      cout<<20<<'\n';
+    }
+    else if (n==3)
+    {
+      if (a[0][0]==1)
+      {
+        cout<<5<<'\n';
+      }
+      else
+      cout<<3<<'\n';
+    }
 }
 
 
@@ -24,7 +52,7 @@ ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
   ll t=1;
-  //cin>>t;
+  cin>>t;
     while(t--)
      {
        solve();
