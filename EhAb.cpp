b5ee1aa902f11d9920b0282a5d544d5907717bc1
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-12-18 14:53:08 +0600

    12/18/23

--- EhAb.cpp
+++ EhAb.cpp
@@ -7,9 +7,40 @@ using ll=long long ;
 
 void solve(void)
 {
-    ll n;
+    ll od=0,ev=0,n;
     cin>>n;
-    cout<<"1"<<" "<<(n-1)<<"\n";
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+        if (o%2==0)
+        {
+          ev=1;
+        }
+        else
+        {
+          od=1;
+        }
+        
+    }
+    if ((ev==0 && od==1)||(ev==1 && od==0))
+    {
+      for(auto element : v)
+      {
+       cout<<element<<" ";
+      }
+      cout<<'\n';
+      return;
+    }
+    
+    sort(v.begin(),v.end());
+    for(auto element : v)
+    {
+    cout<<element<<" ";
+    }
+    cout<<'\n';
 }
 
 
@@ -18,8 +49,8 @@ int main()
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
-  int t;
-  cin>>t;
+  int t=1;
+  //cin>>t;
     while(t--)
      {
        solve();
--- modular.cpp
+++ modular.cpp
@@ -15,12 +15,12 @@ void solve(void)
 {
     ll c=0,a,b;
     cin>>a>>b;
-    if (a==0)
+    if (a==0 && b==0)
     {
         cout<<0<<'\n';
         return;
     }
-    if (b==0)
+    if (b==0 && a!=0)
     {
         cout<<1<<'\n';
         return;
