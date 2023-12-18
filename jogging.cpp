39b45712767f989c7c88f8776c73fc26242f4667
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-12-18 09:16:20 +0600

    12/18/23

--- jogging.cpp
+++ jogging.cpp
@@ -1,62 +1,29 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-const int inf = 3e5;
-using ll=unsigned long long ;
 
-void solve(void)
-{
-    ll a,b,c,d,e,f,x;
-    cin>>a>>b>>c>>d>>e>>f>>x;
-    ll aoki=0,taka=0;
-    ll c_time=x;
+int main() {
+    int A, B, C, D, E, F, X;
+    cin >> A >> B >> C >> D >> E >> F >> X;
 
-    while (true)
-    {
-      taka+=(a*b);
-      cout<<taka<<" ";
-      c_time-=a;
-      if (c_time)
-      {
-        break;
-      }
-      c_time-=c;
-      
-    }
-    c_time=x;
-    while (true)
-    {
-      aoki+=(d*e);
-      cout<<aoki<<" ";
-      c_time-=d;
-      if (c_time>x)
-      {
-        break;
-      }
-      c_time-=f;
-      
+    int takahashiDistance = 0;
+    int aokiDistance = 0;
+
+    for (int i = 0; i < X; i++) {
+        if (i % (A + C) < A) {
+            takahashiDistance += B;
+        }
+        if (i % (D + F) < D) {
+            aokiDistance += E;
+        }
     }
 
-    if(taka>aoki)
-    cout<<"Takahashi"<<'\n';
+    if (takahashiDistance > aokiDistance) {
+        cout << "Takahashi" << endl;
+    } else if (aokiDistance > takahashiDistance) {
+        cout << "Aoki" << endl;
+    } else {
+        cout << "Draw" << endl;
+    }
 
-    else if(taka<aoki)
-    cout<<"Aoki"<<'\n';
-
-    else
-    cout<<"Draw"<<'\n';
-}
-
-
-int main()
-{
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-
-  ll t=1;
-  //cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    return 0;
 }
