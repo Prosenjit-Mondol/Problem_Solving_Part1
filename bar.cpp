bf436294822cbb54f1be44bf90c940f60f1ce116
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-12-24 17:03:11 +0600

    12/24/23

--- bar.cpp
+++ bar.cpp
@@ -13,30 +13,38 @@ ll mod = 10e9 + 7;
 
 void solve(void)
 {
-    int f=0,c=0,n;
-    cin>>n;
-
-    string s[12]={"ABSINTH","BEER","BRANDY","CHAMPAGNE","GIN","RUM","SAKE","TEQUILA","VODAK","WHISKEY","WINE"};
-    string q[n+1];
-    for (int i = 0; i < n; i++)
-    {
-        cin>>q[i];
-    }
-
-
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j= 0; j< 11; j++)
-        {
-            if (q[i]==s[j])
-            {
-                f=1;
-                c++;
-                cout<<q[i]<<" ";
+    string str[] = {"ABSINTH", "BEER", "BRANDY", "CHAMPAGNE", "GIN", "RUM", "SAKE", "TEQUILA", "VODKA", "WHISKEY", "WINE"};
+    int n;
+    cin >> n;
+    string s; 
+ 
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >>s;
+ 
+        bool isNumber = true;
+        for (char c : s) {
+            if (!isdigit(c)) {
+                isNumber = false;
+                break;
+            }
+        }
+        if (isNumber) {
+            int val = stoi(s);
+            if (val < 18) {
+                cnt++;
+            }
+        }
+        else {
+            for (int j = 0; j <11; ++j) {
+                if (s == str[j]) {
+                    cnt++;
+                    break;
+                }
             }
         }
     }
+    cout << cnt << '\n';
 }
 
 
