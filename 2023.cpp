e5df74902cd94c86fdd34dfb6a02db9469949e3f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2023-12-30 22:02:08 +0600

    12/30/23

---
+++ 2023.cpp
@@ -0,0 +1,56 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int p=1,n,k;
+    cin>>n>>k;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+        p*=o;
+    }
+    if (p==2023)
+    {
+        cout<<"YES"<<'\n';
+        for (int i = 0; i < k; i++)
+        {
+            cout<<1<<" ";
+        }
+        cout<<'\n';
+    }
+    else if (n==k)
+    {
+        
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
