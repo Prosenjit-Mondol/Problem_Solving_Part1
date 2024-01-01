c17b7b324b3949a4675520240a9a4a1a11df9a65
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-01-01 12:34:02 +0600

    1/1/24

---
+++ two_divisor.cpp
@@ -0,0 +1,44 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll a,b;
+    cin>>a>>b;
+    if (b%a)
+    {
+        ll x=b*(a/(__gcd(a,b)));
+        cout<<x<<'\n';
+    }
+    else
+    {
+        ll x=b*(b/a);
+        cout<<x<<'\n';
+    }
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
