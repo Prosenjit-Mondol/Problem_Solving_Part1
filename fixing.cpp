9e45728a0a3f7b80d96e110d5509606d780e247f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-01-01 12:21:06 +0600

    1/1/2024

--- fixing.cpp
+++ fixing.cpp
@@ -1,4 +1,3 @@
-
 //Author  :  PROSENJIT MONDOL
 
 
@@ -14,35 +13,9 @@ ll mod = 10e9 + 7;
 
 void solve(void)
 {
-    int n;
     string s;
-    cin>>n>>s;
-    ll total=0;
-    vector<ll>v;
-    for (int i = 0; i < n; i++)
-    {
-        if (s[i]=='L')
-        {
-            v.push_back((n-i-1)-i);
-            total+=i;
-        }
-        else
-        {
-            v.push_back(i-(n-1-i));
-            total+=(n-1-i);
-        }
-          
-    }
-    sort(v.begin(),v.end(),greater<ll>());
-    for (int i = 0; i <n; i++)
-    {
-       if (v[i]>0)
-       {
-        total+=v[i];
-       }
-       cout<<total<<" ";
-    }
-    cout<<'\n';
+    cin>>s;
+    
 }
 
 
@@ -53,7 +26,7 @@ ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
   ll t=1;
-  cin>>t;
+  //cin>>t;
     while(t--)
      {
        solve();
