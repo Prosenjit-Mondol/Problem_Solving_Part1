913ba994b9cd31ef700cc45f09aad7812e23a297
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-01-04 15:56:26 +0600

    1/4/24

--- morning3.cpp
+++ morning3.cpp
@@ -1,3 +1,4 @@
+
 //Author  :  PROSENJIT MONDOL
 
 
@@ -13,8 +14,36 @@ ll mod = 10e9 + 7;
 
 void solve(void)
 {
-  int k='1'-'8'+1;
-  cout<<k;
+    int r=0,k;
+    string s;
+    cin>>s;
+    int a,b,c,d;
+    a=s[0]-'0';
+    if (a==0)
+    {
+        a=10;
+    }
+    
+    b=s[1]-'0';
+    if (b==0)
+    {
+        b=10;
+    }
+    
+    c=s[2]-'0';
+    if (c==0)
+    {
+        c=10;
+    }
+    
+    d=s[3]-'0';
+    if (d==0)
+    {
+        d=10;
+    }
+    
+    r=abs(a-1)+abs(b-a)+abs(c-b)+abs(d-c);
+    cout<<r+4<<'\n';
 }
 
 
@@ -25,7 +54,7 @@ ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
   ll t=1;
-  //cin>>t;
+  cin>>t;
     while(t--)
      {
        solve();
--- raspberries.cpp
+++ raspberries.cpp
@@ -13,8 +13,39 @@ ll mod = 10e9 + 7;
 
 void solve(void)
 {
-  int k='1'-'8'+1;
-  cout<<k;
+    int va,c=0,m=mod,n,k;
+    cin>>n>>k;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+        if (o%2==0)
+        {
+            c++;
+        }
+        if (o%k==0)
+        {
+            m=0;
+        }
+        va=o%k;
+        m=min(m,k-va);
+    }
+    if (k==4)
+    {
+        if (c>=2)
+        {
+            m=0;
+        }
+        else if (c==1)
+        {
+            m=min(m,1);
+        }
+        else
+        m=min(m,2);
+    }
+    cout<<m<<'\n';
 }
 
 
@@ -25,7 +56,7 @@ ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 
   ll t=1;
-  //cin>>t;
+  cin>>t;
     while(t--)
      {
        solve();
