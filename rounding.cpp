150b46305ac453fabfc00209593eed6fdd5023d7
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-01-18 23:15:47 +0600

    1/18/24

---
+++ rounding.cpp
@@ -0,0 +1,46 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n;
+    cin>>n;
+    int r=n%10;
+    int d=10-r;
+    if (d<=5)
+    {
+        cout<<(n+d)<<'\n';
+    }
+    else
+    {
+
+        cout<<(n-r)<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
