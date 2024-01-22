bbee99398933a31d4ffe21f3334f227e843a78cf
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-01-23 03:23:00 +0600

    1/23/24

---
+++ fox_number_game.cpp
@@ -0,0 +1,44 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int mi=0,n;
+    cin>>n;
+    vector<int>v;
+    for(int i=0;i<n;i++)
+    {
+        int o;
+        cin>>o;
+        v.push_back(o);
+        mi=__gcd(mi,o);
+    }
+    cout<<(mi*n)<<'\n';
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
