5b349e8ff84bbd85f02c689c16337371336e2064
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-01-28 17:32:10 +0600

    1/28/24

--- zzz.cpp
+++ zzz.cpp
@@ -1,34 +1,39 @@
-//Author  :  PROSENJIT MONDOL
-
+//Author: PROSENJIT MONDOL
 
 #include<bits/stdc++.h>
-#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
-#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
+
 const int inf = 3e5;
-using ll=unsigned long long ;
+using ll = unsigned long long;
 ll mod = 10e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-  int k='1'-'8'+1;
-  cout<<k;
+  int c=0;
+  for (int i = 0; i < 10; i++)
+  {
+   for (int j = 0; j< i; j++)
+   {
+    c++;
+   }
+   
+  }
+  cout<<c;
 }
-
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  ll t=1;
-  //cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    ll t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
