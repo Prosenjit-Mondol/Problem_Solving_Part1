0dd436129c581840cc385fea475ab41df59521a2
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-02 00:52:08 +0600

    2/2/24

---
+++ balance_Problemset.cpp
@@ -0,0 +1,45 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll x,r,n;
+    cin>>x>>n;
+    r=x/n;
+    while (1)
+    {
+        if (x%r==0)
+        {
+            cout<<r<<'\n';
+            return;
+        }
+        r--;
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
