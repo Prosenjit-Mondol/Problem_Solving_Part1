4da4214e9f69a64fcd0f6b310bf0dbcdc23ffa75
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-06 12:16:57 +0600

    6/2/2024

---
+++ Cowardly_rooks.cpp
@@ -0,0 +1,48 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n,m;
+    cin>>n>>m;
+    for (int i = 0; i < m; i++)
+    {
+        int a,b;
+        cin>>a>>b;
+    }
+    if (n>m)
+    {
+        cout<<"YES"<<'\n';
+    }
+    else
+    {
+        cout<<"NO"<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
