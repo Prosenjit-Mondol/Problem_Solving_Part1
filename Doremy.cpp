ecda603b42ccf2a476bc72b86ac0fd75a383a98b
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-06 12:56:08 +0600

    6/2/2024

---
+++ Doremy.cpp
@@ -0,0 +1,43 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    ll l=v[n-1]/v[0];
+    cout<<l<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
