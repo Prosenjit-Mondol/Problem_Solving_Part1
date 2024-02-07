08a3961caf96b93023a0e43adb91366c0707b0d1
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-07 09:05:16 +0600

    2/7/24

--- pair_of_toy.cpp
+++ pair_of_toy.cpp
@@ -15,6 +15,20 @@ void solve(void)
 {
     ll n,k,c=0;
     cin>>n>>k;
+    if (k/2>n)
+    {
+      cout<<0<<'\n';
+    }
+    else if (k<n)
+    {
+      cout<<(k/2)<<'\n';
+    }
+    else
+    {
+      c=(k-1)/2;
+      c++;
+      cout<<(n-c+1)<<'\n';
+    }
     
 }
 
