09562cfccecc5d91332a5023db050797b800be96
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-12 16:49:13 +0600

    2/12/24

---
+++ choosing_symbol_pairs.cpp
@@ -0,0 +1,40 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    string s;
+    cin>>s;
+    set<char>se;
+    for(int i=0;i<s.length();i++)
+    {
+    se.insert(s[i]);
+    }
+    cout<<s.size()<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
