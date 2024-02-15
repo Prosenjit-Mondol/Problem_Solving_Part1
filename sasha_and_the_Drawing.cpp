111cf1b6d4f1beecc7c72b91008de0fa3d7744b4
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-15 21:16:30 +0600

    2/15/24

---
+++ sasha_and_the_Drawing.cpp
@@ -0,0 +1,51 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=unsigned long long ;
+ll mod = 10e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n,k;
+    cin>>n>>k;
+    if (k%2!=0)
+    {
+        cout<<(k/2+1)<<'\n';
+    }
+    else
+    {
+        ll d=k/2;
+        if (d<=n)
+        {
+            cout<<d<<'\n';
+        }
+        else
+        {
+            cout<<(d+1)<<'\n';
+        }
+        
+    }
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
