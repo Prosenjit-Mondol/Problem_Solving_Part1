38332e998a1a3dfe612c4ce06f6e1aefc56f7df5
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-18 21:19:42 +0600

    2/18/24

---
+++ Thorns_Coins.cpp
@@ -0,0 +1,52 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int c=0,m=0;
+
+    for (int i = 1; i <n; i++)
+    {
+        if (s[i]=='@')
+        {
+           c++;
+        }
+        m=max(m,c);
+        if (s[i]=='*'&&s[i-1]!='.')
+        {
+            c--;
+        }
+        
+    }
+    cout<<m<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
