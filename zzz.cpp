9c2cddf250172ed9dd718aa6489f7dd46a604650
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-20 12:22:32 +0600

    2/20/24

--- zzz.cpp
+++ zzz.cpp
@@ -5,30 +5,11 @@
 using namespace std;
 
 int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n;
-        cin >> n;
-
-        vector<int> trace(n);
-        for (int i = 0; i < n; ++i) {
-            cin >> trace[i];
-        }
-
-        string s(n, 'a');
-
-        for (int i = 1; i < n; ++i) {
-            if (trace[i] == 0) {
-                s[i] = s[i - 1] == 'a' ? 'b' : 'a';
-            } else {
-                s[i] = s[trace[i]];
-            }
-        }
-
-        cout << s << endl;
+    vector<int>v={2,5,8,6};
+    vector<int>vv={2,56,5};
+    vv.insert(vv.end(),v.begin(),v.end());
+    for(auto element :vv)
+    {
+    cout<<element<<" ";
     }
-
-    return 0;
 }
