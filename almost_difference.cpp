617a5084bbd3f81c0a40ca2d671ff17d216e8db5
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-22 12:07:46 +0600

    2/22/24

---
+++ almost_difference.cpp
@@ -0,0 +1,55 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll sum=0,n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j=i+1; j<n; j++)
+        {
+            if (abs(v[i]-v[j])>1)
+            {
+                sum+=(v[j]-v[i]);
+            }
+            
+        }
+        
+    }
+    cout<<sum<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
