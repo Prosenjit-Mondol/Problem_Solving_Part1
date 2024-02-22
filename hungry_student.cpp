aaaa671e1145b05b44e222af5c4bb173d35385ce
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-22 13:14:22 +0600

    2/22/24

---
+++ hungry_student.cpp
@@ -0,0 +1,44 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    int x=n%7;
+    if (x%3)
+    {
+        cout<<"NO"<<'\n';
+    }
+    else
+    {
+        cout<<"YES"<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
