ff802a8fa4c6466e47b2367b0f969b0565a8de52
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-22 11:52:01 +0600

    2/22/24

---
+++ boxes_packing.cpp
@@ -0,0 +1,43 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int m=0,n;
+    cin>>n;
+    map<int,int>mp;
+    for (int  i = 0; i <n; i++)
+    {
+        ll x;
+        cin>>x;
+        mp[x]++;
+        m=max(m,mp[x]);
+    }
+    cout<<m<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
