7c1c2e901ecf350aaab4bdf240929ab791076f07
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-24 11:28:41 +0600

    2/24/24

---
+++ second_O_statistic.cpp
@@ -0,0 +1,50 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    set<int>s;
+    for(int i=0;i<n;i++)
+    {
+    int x;
+    cin>>x;
+    s.insert(x);
+    }
+    if (s.size()<2)
+    {
+        cout<<"NO"<<'\n';
+    }
+    else
+    {
+        cout<<*(++s.begin())<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
