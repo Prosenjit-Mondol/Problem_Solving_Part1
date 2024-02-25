8306c00fc7756f08b21dda9e2b87ff5e70b8bb50
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-25 13:46:40 +0600

    2/25/24

---
+++ balance_bracket.cpp
@@ -0,0 +1,53 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int sum=0,n;
+    cin>>n;
+    string s;
+    cin>>s;
+    stack<char>st;
+    for (int i = 0; i <n; i++)
+    {
+        if (s[i]=='(')
+        {
+           st.push(s[i]);
+        }
+        int x=st.size();
+        sum=max(sum,x);
+        if (s[i]==')')
+        {
+           st.pop();
+        }
+        
+        
+    }
+    cout<<sum<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
