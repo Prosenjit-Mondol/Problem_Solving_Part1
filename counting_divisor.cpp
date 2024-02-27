5a07e36dbc5933c1820cef4c7e2fcf6f3f32babf
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-02-27 10:36:50 +0600

    2/27/24

---
+++ counting_divisor.cpp
@@ -0,0 +1,43 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    int d=0;
+    for (int i = 1; i <=n; i++)
+    {
+        if(n%i==0){
+            d+=i*i==n?1:2;
+        }
+        cout<<d<<" ";
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
