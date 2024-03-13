f520ff43eddea2c242fca388361ffd0cc44e5690
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-14 01:18:46 +0600

    3/14/24

---
+++ Anti_light.cpp
@@ -0,0 +1,47 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n,m;
+    cin>>n>>m;
+    if (n==1&&m==1)
+    {
+        cout<<0<<'\n';
+    }
+    else if ((n==1&&m>1)||(m==1&&n>1))
+    {
+        cout<<1<<'\n';
+    }
+    else
+    {
+        cout<<2<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
