4e1dac8a9b44a6f7e2da923e2ffc7a7a10565f7c
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-15 16:33:44 +0600

    3/15/24

---
+++ upper_bound_function.cpp
@@ -0,0 +1,59 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    sort(v.begin(),v.end());
+    int f;
+    cin>>f;
+    int lo=0,hi=n-1,mi,ans=n;
+    while (lo<=hi)
+    {
+        mi=(lo+hi)/2;
+        if(v[mi]>f){
+            ans=mi;
+            hi=mi-1;
+        }
+        else
+        {
+            lo=mi+1;
+        }
+    }
+
+    cout<<ans<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
