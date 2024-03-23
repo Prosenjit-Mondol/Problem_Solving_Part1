805b41feec17b9b22c252652e761cb28faff1995
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-23 20:20:58 +0600

    3/23/24

--- median_array.cpp
+++ median_array.cpp
@@ -1,52 +1,66 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-    int n;
-    cin>>n;
-    set<ll>s;
-    for(int i=0;i<n;i++)
+    int cnt,st, n;
+    cin >> n;
+    vector<ll> v;
+    for (int i = 0; i < n; i++)
     {
-    ll x;
-    cin>>x;
-    s.insert(x);
+        ll o;
+        cin >> o;
+        v.push_back(o);
     }
-    if (n==1)
+    sort(v.begin(), v.end());
+    if (n % 2)
     {
-        cout<<1<<'\n';
-        return;
+        st = n / 2;
+        cnt=st+1;
     }
-    if (n==2&&s.size()==1)
+    else
     {
-        cout<<2<<'\n';
-        return;
-    }
-    cout<<(n-s.size())<<'\n';
-}
+        st = n / 2;
+        cnt=st+1;
+        st -= 1;
 
+    }
+    set<int> s;
+    for (int i = st; i < n; i++)
+    {
+        s.insert(v[i]);
+    }
+    if (s.size() ==cnt)
+    {
+        cout << 1 << '\n';
+    }
+    else
+    {
+        cnt=cnt-s.size()+1;
+        cout << cnt << '\n';
+    }
+}
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
