e19bbcad073b09488eb767be39422420a600b2b9
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-26 17:57:28 +0600

    3/26/24

---
+++ coin_piles.cpp
@@ -0,0 +1,45 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll a,b;
+    cin>>a>>b;
+    if (b>a)
+    {
+        swap(a,b);
+    }
+    
+    if (a>2*b||(a+b)%3!=0)
+    {
+        cout<<"NO"<<'\n';
+    }
+    else
+    cout<<"YES"<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
