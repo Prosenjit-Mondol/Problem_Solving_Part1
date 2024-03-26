0b7130d0594247e30f2c3b0ea035f5f557bdd02a
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-26 17:26:53 +0600

    3/26/24

---
+++ trailing_zero.cpp
@@ -0,0 +1,39 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll c=0,n;
+    cin>>n;
+    for (ll i = 5; n/i>=1; i*=5)
+    {
+        c+=(n/i);
+    }
+    cout<<c<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
