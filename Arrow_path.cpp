91c0bcbb756ddd9da36813021cef7bf1d65be50a
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-28 01:11:36 +0600

    28/03/24

--- Arrow_path.cpp
+++ Arrow_path.cpp
@@ -1,51 +1,75 @@
-//Author  :  PROSENJIT MONDOL
-
-
 #include<bits/stdc++.h>
-#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
-#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
-using namespace std;
-const int inf = 3e5;
-using ll=long long ;
-ll mod = 1e9 + 7;
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+using namespace std; 
 
-//------------------------------------------------------------------------------
+const int N = 2e5+10;
 
-void solve(void)
-{
-    int n;
-    cin>>n;
-    string s1,s2;
-    cin>>s1>>s2;
-    map<int,int>mp;
-    if (s1[0]=='<')
-    {
-      
-    }
-    
-    for (int i = 1; i <n; i++)
-    {
-      if (s1[i]=='<')
-      {
-        mp[i-1]=i;
-        break;
-      }
-    }
-    
+char ar[2][N];
+int n;
+
+bool isvalid(int x, int y){
+   return (x>=0 && x<2 && y>=0 && y<n);
 }
 
+int dx[] = {0, 0, -1, 1};
+int dy[] = {1, -1, 0, 0};
 
-//------------------------------------------------------------------------------
-int main()
-{
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+int vis[2][N];
 
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+void dfs(int x, int y){
+   if(!isvalid(x, y))return;
+
+   if(vis[x][y])return;
+   vis[x][y] = 1;
+
+   for(int ii=0; ii<4; ii++){
+      int a = x+dx[ii];
+      int b = y+dy[ii];
+      if(isvalid(a, b)){
+         if(ar[a][b]=='>'){
+            b++;
+         }
+         else b--;
+         dfs(a, b);
+      }
+   }
+
+}
+
+int main() {
+   FAST_IO;
+   // start
+   int t;
+   cin>>t;
+   read:
+   while (t--) {
+      cin>>n;
+
+      for(int i=0; i<2; i++){
+         for(int j=0; j<n+5; j++){
+            vis[i][j] = 0;
+         }
+      }
+
+      for(int i=0; i<2; i++){
+         for(int j=0; j<n; j++){
+            cin>>ar[i][j];
+         }
+      }
+
+      dfs(0, 0);
+
+      int res = vis[1][n-1];
+
+      if(res)cout<<"YES\n";
+      else cout<<"NO\n";
+
+
+   
+   }
+
+
+
+   return 0;
 }
