bdb4f38a9a415287dff2767ebc5865688fe4b019
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-28 00:03:16 +0600

    28/03/24

--- apple_division.cpp
+++ apple_division.cpp
@@ -13,8 +13,35 @@ ll mod = 1e9 + 7;
 
 void solve(void)
 {
-    int n=2&;
-    cout<<n;
+    int n;
+    cin>>n;
+    ll ans=1e18;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    for (int i = 0; i <(1<<n); i++)
+    {
+        ll s1=0,s2=0;
+        for (int p= 0; p<n; p++)
+        {
+            if (i&(1<<p))
+            {
+                s1+=v[p];
+            }
+            else
+            {
+                s2+=v[p];
+            }
+            
+        }
+        ans=min(ans,abs(s1-s2));
+    }
+    
+    cout<<ans<<'\n';
 }
 
 
