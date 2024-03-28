8c6dd76dd36bf2663477ee9bf3ab3333aafb3d6f
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-28 13:05:50 +0600

    28/03/24

--- Apartment.cpp
+++ Apartment.cpp
@@ -1,41 +1,68 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-    ll n,m,k;
-    cin>>n>>m>>k;
-    vector<ll>v;
-    for(int i=0;i<n;i++)
+  ll n, m, k;
+  cin >> n >> m >> k;
+  vector<ll> vn;
+  for (int i = 0; i < n; i++)
+  {
+    ll o;
+    cin >> o;
+    vn.push_back(o);
+  }
+  sort(vn.begin(), vn.end());
+  vector<ll> vm;
+  for (int i = 0; i < m; i++)
+  {
+    ll o;
+    cin >> o;
+    vm.push_back(o);
+  }
+  sort(vm.begin(), vm.end());
+  int pa=0,pb=0,ans=0;
+  while (pa<n&&pb<m)
+  {
+    if (abs(vn[pa]-vm[pb])<=k)
     {
-        ll o;
-        cin>>o;
-        v.push_back(o);
+      ans++;
+      pa++;
+      pb++;
     }
+    else if (vn[pa]<vm[pb])
+    {
+      pa++;
+    }
+    else
+    {
+      pb++;
+    }
+    
+  }
+  cout<<ans<<'\n';
 }
-
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
 
-  ll t=1;
-  //cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+  ll t = 1;
+  // cin>>t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
 }
---
+++ playlist.cpp
@@ -0,0 +1,53 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int mx=0,n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    for (int i = 0; i <n-1; i++)
+    {
+        for (int j = i+1; j<n; j++)
+        {
+            if (v[i]==v[j])
+            {
+                break;
+            }
+            mx=max(mx,(j-i));
+        }
+    }
+    cout<<mx<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
