885860b97438232bd5865198c9fb99f5e58f7905
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-29 11:27:59 +0600

    3/29/24

--- product_binary_decimal.cpp
+++ product_binary_decimal.cpp
@@ -1,74 +1,87 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
-bool isBinaryDecimal(int num) {
-    while (num > 0) {
-        int digit = num % 10;
-        if (digit != 0 && digit != 1)
-            return false;
-        num /= 10;
+// bool isBinaryDecimal(int num) {
+//     while (num > 0) {
+//         int digit = num % 10;
+//         if (digit != 0 && digit != 1)
+//             return false;
+//         num /= 10;
+//     }
+//     return true;
+// }
+
+vector<ll> v;
+
+ll toBinary(ll x)
+{
+    vector<ll> tmp;
+    while (x > 0)
+    {
+        tmp.push_back(x % 2);
+        x /= 2;
     }
-    return true;
+
+    ll val = 0;
+    ll p = 1;
+    for (int i = 0; i < tmp.size(); i++)
+    {
+        val += (tmp[i] * p);
+        p *= 10;
+    }
+    return val;
 }
 
 void solve(void)
 {
+    
     int n;
-    cin>>n;
-    if (n==1)
+    cin >> n;
+
+    auto it = find(v.begin(), v.end(), n);
+    if (it != v.end()||n == 1)
     {
-        cout<<"YES"<<'\n';
+        cout << "YES" << '\n';
         return;
-    }
-    vector<ll>v;
-    for(int i=10;i<=n;i++)
-    {
-        if (isBinaryDecimal(i))
-        v.push_back(i);
     }
 
-    auto it=find(v.begin(),v.end(),n);
-    if(it!=v.end()){
-        cout<<"YES"<<'\n';
-        return;
-    }
-    for (int i = 0; i < v.size(); i++)
+    for (int i = 1; i < v.size(); i++)
     {
-        while (n%v[i]==0)
+        while (n % v[i] == 0)
         {
-            n/=v[i];
-            if (n==1)
+            n /= v[i];
+            if (n == 1)
             {
-                cout<<"YES"<<'\n';
+                cout << "YES" << '\n';
                 return;
             }
-            
         }
-        
     }
-    cout<<"NO"<<'\n';
+    cout << "NO" << '\n';
 }
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    for (int i = 1; i < 35; i++)
+    {
+        v.push_back(toBinary(i));
+    }
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
