c660398bbd008ba6a485c18cfc6c87bf400c7bf9
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-30 22:43:38 +0600

    3/30/24

---
+++ avtobus.cpp
@@ -0,0 +1,52 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n;
+    cin>>n;
+    if (n%2||n==2)
+    {
+        cout<<-1<<'\n';
+    }
+    else
+    {
+        ll ma,mi;
+        
+        ma=n/4;
+        mi=n/6;
+        if (n%6)
+        {
+            mi++;
+        }
+        
+        cout<<mi<<" "<<ma<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
