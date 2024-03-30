db240221de9a407eab8cc0674c49c73c21e5e5cf
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-30 23:01:34 +0600

    3/30/24

---
+++ stone_age_problem.cpp
@@ -0,0 +1,46 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n,q;
+    cin>>n>>q;
+    vector<ll>v(n,10);
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    v(n,10);
+    for(auto element : v)
+    {
+    cout<<element<<" ";
+    }
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
