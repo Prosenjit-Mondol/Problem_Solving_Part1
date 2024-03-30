635f2befe38d1d6c7163c6c6cb3fb5bbaf309a7c
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-30 20:28:00 +0600

    3/30/24

---
+++ counting_rooms.cpp
@@ -0,0 +1,50 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int c=0,n,m;
+    cin>>n>>m;
+    for (int i = 0; i < n; i++)
+    {
+        set<char>s;
+        for(int i=0;i<m;i++)
+        {
+        char x;
+        cin>>x;
+        s.insert(x);
+        }
+        if (s.size()!=1)
+        {
+            c++;
+        }
+        s.clear();
+    }
+    cout<<c<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
