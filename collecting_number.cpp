12c48bd8bedfaf542f936bd4e9a015e4cf283a8e
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-03-31 15:40:38 +0600

    3/31/24

---
+++ collecting_number.cpp
@@ -0,0 +1,52 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int ma=1,c=0,n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    for (int i = 1; i <n; i++)
+    {
+        if (v[i]>v[i-1])
+        {
+            c=0;
+        }
+        else
+        c++;
+        ma=max(ma,c);
+    }
+    cout<<ma+1<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
