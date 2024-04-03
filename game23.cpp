49d34be928d9080a81101aa68069cfa0352f7756
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-03 13:32:23 +0600

    4/3/24

---
+++ game23.cpp
@@ -0,0 +1,59 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n,m;
+    cin>>n>>m;
+    if (m%n)
+    {
+        cout<<-1<<'\n';
+    }
+    else
+    {
+        int c=0;
+        while (n!=m)
+        {
+            c++;
+            ll x=n*3;
+            if (m%x==0)
+            {
+                n=x;
+            }
+            else
+            {
+                n=x*2;
+            }
+            
+        }
+        cout<<c<<'\n';
+        
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
