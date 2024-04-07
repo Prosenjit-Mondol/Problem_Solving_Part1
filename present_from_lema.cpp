d3b4d4cb47daaa13d0adc1b990873aa6dde349ce
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-07 22:00:42 +0600

    4/7/24

--- present_from_lema.cpp
+++ present_from_lema.cpp
@@ -15,37 +15,37 @@ void solve(void)
 {
     int n;
     cin>>n;
-    for (int i = 0; i <=n; i++)
+    for (int i =0; i <=n; i++)
     {
-        for (int j = 0; j<n-i; j++)
-        {
-          cout<<" ";
-        }
-        for (int j = 0;j<=i; j++)
-        {
-          cout<<j;
-        }
-        for (int j = i-1;j>=0; j--)
-        {
-          cout<<j;
-        }
-        cout<<'\n';
+      for (int j= 1; j<=(n-i)*2; j++)
+      {
+        cout<<" ";
+      }
+      for (int j= 0;j<=i; j++)
+      {
+        cout<<" "<<j;
+      }
+      for (int j=i-1;j>=0; j--)
+      {
+        cout<<" "<<j;
+      }
+      cout<<'\n';
     }
     for (int i =n-1; i>=0; i--)
     {
-        for (int j = 0; j<n-i; j++)
-        {
-          cout<<" ";
-        }
-        for (int j = 0;j<=i; j++)
-        {
-          cout<<j;
-        }
-        for (int j = i-1;j>=0; j--)
-        {
-          cout<<j;
-        }
-        cout<<'\n';
+      for (int j= 1; j<=(n-i)*2; j++)
+      {
+        cout<<" ";
+      }
+      for (int j= 0;j<=i; j++)
+      {
+        cout<<" "<<j;
+      }
+      for (int j=i-1;j>=0; j--)
+      {
+        cout<<" "<<j;
+      }
+      cout<<'\n';
     }
 }
 
