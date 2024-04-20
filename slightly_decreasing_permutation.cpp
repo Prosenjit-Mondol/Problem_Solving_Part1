92a2224530592757a49e1c347f61627185799ccd
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-20 13:29:49 +0600

    4/20/24

---
+++ slightly_decreasing_permutation.cpp
@@ -0,0 +1,45 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll i,n,k;
+    cin>>n>>k;
+    int nn=n;
+    for (i =1; i<=k; i++)
+    {
+        cout<<n<<" ";
+        n--;
+    }
+    for (int j= 1; j<=(nn-k); j++)
+    {
+        cout<<j<<" ";
+    }
+    cout<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
