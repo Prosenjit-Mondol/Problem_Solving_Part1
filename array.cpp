7b1a7f0cbcff750a0a6cabcad83d257ceb0a956b
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-21 16:16:01 +0600

    4/21/24

--- array.cpp
+++ array.cpp
@@ -1,87 +1,120 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-    int z=0,n;
-    cin>>n;
-    vector<ll>vp,vn;
-    for(int i=0;i<n;i++)
+    int z = 0, n;
+    cin >> n;
+    vector<ll> vp, vn;
+    for (int i = 0; i < n; i++)
     {
         ll o;
-        cin>>o;
-        if (o>0)
+        cin >> o;
+        if (o > 0)
         {
             vp.push_back(o);
         }
-        else if(o<0)
-        vn.push_back(o);
+        else if (o < 0)
+            vn.push_back(o);
         else
-        z++;
+            z++;
     }
 
-    if (vn.size()==0)
+    if (vn.size()==0&&vp.size()==0)
     {
-        cout<<vp[0]<<" "<<-1<<'\n';
-        if(vp.size()<1)
-        cout<<2<<" "<<1<<'\n';
-        else
-        cout<<2<<" "<<vp[1]<<'\n';
-    }
-    else if (vp.size()==0)
-    {
-        cout<<1<<" "<<vn[0]<<'\n';
-        if(vn.size()<1)
-        cout<<2<<" "<<1<<'\n';
-        else
-        cout<<2<<" "<<vn[1]<<" "<<vn[2]<<'\n';
-    }
-    else
-    {
-        cout<<1;
-        for (int i = 0; i <vn.size(); i++)
+        cout << 1 << " " << -1 << '\n';
+        cout << 2 << " " << 1 << '\n';
+        cout << 1<<" "<<0;
+        for (int i = 1; i < z; i++)
         {
-            cout<<" "<<vn[i];
-        }
-        cout<<'\n';
-        cout<<1;
-        for (int i = 0; i <vp.size(); i++)
-        {
-            cout<<" "<<vp[i];
+            cout << " " << 0;
         }
         cout<<'\n';
     }
     
-    cout<<1;
-    for (int i = 0; i <z; i++)
+    else if (vn.size() == 0)
     {
-        cout<<" "<<0;
-    }
-    cout<<'\n';
-}
+        cout << 1 << " " << -1 << '\n';
+        cout << 2;
+        for (int i = 0; i < vp.size(); i++)
+        {
+            cout << " " << vp[i];
+        }
+        cout << '\n';
+        cout << 1<<" "<<0;
+        for (int i = 1; i < z; i++)
+        {
+            cout << " " << 0;
+        }
+        cout<<'\n';
 
+    }
+
+    else if (vp.size() == 0)
+    {
+        cout << 1 << " " << vn[0] << '\n';
+        if (vn.size() > 3)
+            cout << 2 << " " << vn[1] << " " << vn[2]<<'\n';
+        else
+            cout << 2 << " " << 1 << '\n';
+
+        cout << 1<<" "<<0;
+        for (int i = 1; i < z; i++)
+        {
+            cout << " " << 0;
+        }
+        for (int i = 1; i < vn.size(); i++)
+        {
+            cout << " " << vn[i];
+        }
+        cout << '\n';
+    }
+
+
+    else
+    {
+        cout << 1 << " " << vn[0] << '\n';
+
+        cout << 1;
+        for (int i = 0; i < vp.size(); i++)
+        {
+            cout << " " << vp[i];
+        }
+        cout << '\n';
+
+        cout << 1<<" "<<0;
+        for (int i = 1; i < z; i++)
+        {
+            cout << " " << 0;
+        }
+        for (int i = 1; i < vn.size(); i++)
+        {
+            cout << " " << vn[i];
+        }
+        cout << '\n';
+    }
+}
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  ll t=1;
-  //cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    ll t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
