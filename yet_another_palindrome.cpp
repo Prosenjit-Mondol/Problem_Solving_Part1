e77ed53212f7c834956b139905a4048d3defc5a1
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-23 15:15:43 +0600

    4/23/24

---
+++ yet_another_palindrome.cpp
@@ -0,0 +1,57 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    for (int i = 1; i <n-1; i++)
+    {
+        if (v[i]!=v[i-1]&&v[i]!=v[i+1])
+        {
+            cout<<"YES"<<'\n';
+            return;
+        }
+        if (v[i]==v[i-1]&&n!=3)
+        {
+            if (v[i-2]==v[i+1]){
+                cout<<"YES"<<'\n';
+                return;
+            }
+        }
+    }
+    cout<<"NO"<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
