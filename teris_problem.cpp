0b68a32b843ee86b5db988d1864fc3473c8693bb
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-24 17:22:50 +0600

    4/24/24

---
+++ teris_problem.cpp
@@ -0,0 +1,51 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i]<v[i-1])
+        {
+            cout<<"NO"<<'\n';
+            return;
+        }
+        
+    }
+     cout<<"YES"<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
