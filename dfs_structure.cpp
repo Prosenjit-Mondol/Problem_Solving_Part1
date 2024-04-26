3f8f48870e59e26e30406a662133574b13c97976
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-27 00:21:30 +0600

    4/27/24

---
+++ dfs_structure.cpp
@@ -0,0 +1,46 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+const int N=1e5+10;
+
+vector<int>g[N];
+//------------------------------------------------------------------------------
+bool vis[N];
+
+void dfs(int vertex){
+    cout<<vertex<<'\n';
+    vis[vertex]=true;
+    for(int child:g[vertex]){
+        cout<<"parent "<<vertex<<" "<<"child "<<child<<"\n";
+        if (vis[child])
+        {
+            continue;
+        }
+        dfs(child);
+    }
+}
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+   int n,m;
+    cin>>n>>m;
+    for (int i = 0; i < m; i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    dfs(1);
+return 0;
+}
