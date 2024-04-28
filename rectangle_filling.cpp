064f5901f7ca8188c5312006ce569593e4bbab97
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-04-29 01:51:02 +0600

    29/4/24

--- rectangle_filling.cpp
+++ rectangle_filling.cpp
@@ -1,129 +1,97 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-    int ub=0,uw=0,ab=0,aw=0,lb=0,lw=0,rb=0,rw=0;
-    string s[1000];
-    int n,m;
-    cin>>n>>m;
-    if (n==1||m==1)
+    int ub = 0, uw = 0, ab = 0, aw = 0, lb = 0, lw = 0, rb = 0, rw = 0;
+    
+    int n, m;
+    cin >> n >> m;
+    char s[n][m + 1];
+    map<ll, ll> mp;
+    for (int i = 0; i < n; i++)
     {
-        if (n==1)
+        for (int j = 0; j < m; j++)
         {
-            set<char>s;
-            for(int i=0;i<m;i++)
-            {
-            char x;
-            cin>>x;
-            s.insert(x);
-            }
-            if (s.size()==1)
-            {
-                cout<<"YES"<<'\n';
-            }
-            else
-            cout<<"NO"<<'\n';
+            cin >> s[i][j];
+            mp[s[i][j]]++;
+        }
+    }
+    if (mp.size() == 1)
+    {
+        cout << "YES" << '\n';
+        return;
+    }
+    
+    for (int i = 0; i < m; i++)
+    {
+        if (s[0][i] == 'B')
+        {
+            ab = 1;
         }
         else
         {
-            for (int i = 0; i <n; i++)
-            {
-                cin>>s[i];
-            }
-            if (s[0]==s[n-1])
-            {
-                cout<<"YES"<<'\n';
-            }
-            else
-            cout<<"NO"<<'\n';
+            aw = 1;
         }
-        return;
+        if (s[n - 1][i] == 'B')
+        {
+            ub = 1;
+        }
+        else
+        {
+            uw = 1;
+        }
     }
-    
     for (int i = 0; i < n; i++)
     {
-        cin>>s[i];
+        if (s[i][0] == 'B')
+        {
+            lb = 1;
+        }
+        if (s[i][0] == 'W')
+        {
+            lw = 1;
+        }
+        if (s[i][m - 1] == 'B')
+        {
+            rb = 1;
+        }
+        if (s[i][m - 1] == 'W')
+        {
+            rw = 1;
+        }
     }
-    if (s[0][0]==s[n-1][n-1])
+    // cout<<ab<<" "<<ub<<" "<<lb<<" "<<rb<<" B"<<aw<<" "<<uw<<" "<<lw<<" "<<rw<<'\n';
+    if ((ab == 1 && ub == 1 && lb == 1 && rb == 1) || (aw == 1 && uw == 1 && lw == 1 && rw == 1))
     {
-        cout<<"YES"<<'\n';
-        return;
+        cout << "YES" << '\n';
     }
     else
     {
-        for (int i = 0; i < m; i++)
-        {
-            if (s[0][i]=='B')
-            {
-                ab=1;
-            }
-            if (s[0][i]=='W')
-            {
-                aw=1;
-            }
-            if (s[n-1][i]=='B')
-            {
-                ub=1;
-            }
-            if (s[n-1][i]=='W')
-            {
-                uw=1;
-            }
-        }
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i][0]=='B')
-            {
-                lb=1;
-            }
-            if (s[i][0]=='W')
-            {
-                lw=1;
-            }
-            if (s[i][n-1]=='B')
-            {
-                rb=1;
-            }
-            if (s[i][n-1]=='W')
-            {
-                rw=1;
-            }
-        }
+        cout << "NO" << '\n';
     }
-    if ((ab==1&&ub==1&&lb==1&&rb==1)||(aw==1&&uw==1&&lw==1&&rw==1))
-    {
-        cout<<"YES"<<'\n';
-    }
-    else
-    {
-        cout<<"NO"<<'\n';
-    }
-    
 }
-
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
