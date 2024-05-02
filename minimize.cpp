67eb8b875f278a628c129a39139816d35535d2d0
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-05-03 01:17:52 +0600

    5/3/24

---
+++ minimize.cpp
@@ -0,0 +1,44 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    for (int i = 2; i*i<n; i++)
+    {
+        if (n%i==0)
+        {
+            cout<<(n/i-1)*(i)<<'\n';
+            return;
+        }
+        
+    }
+    cout<<(n-1)<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
