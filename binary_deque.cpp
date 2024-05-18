387c8d1c58cc52a3d31ffea2cec37a162a97c232
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-05-18 09:53:47 +0600

    5/18/24

--- binary_deque.cpp
+++ binary_deque.cpp
@@ -13,7 +13,67 @@ ll mod = 1e9 + 7;
 
 void solve(void)
 {
-
+  int c=0,n,s,sum=0;
+  cin>>n>>s;
+  vector<int>v;
+  for(int i=0;i<n;i++)
+  {
+      int o;
+      cin>>o;
+      v.push_back(o);
+      sum+=o;
+  }
+  if (sum<s)
+  {
+    cout<<-1<<'\n';
+    return;
+  }
+  if (sum==s)
+  {
+    cout<<0<<'\n';
+    return;
+  }
+  int f=0,l=v.size()-1;
+  while (sum>s)
+  {
+    if (v[f]==1)
+    {
+      f++;
+      c++;
+    }
+    else if (v[l]==1)
+    {
+      l--;
+      c++;
+    }
+    else
+    {
+      int fg=f,lg=l;
+      while (v[fg]!=1)
+      {
+        fg++;
+      }
+      while (v[lg]!=1)
+      {
+        lg--;
+      }
+      if (lg<=fg)
+      {
+        c+=(l-lg);
+        l=lg-1;
+      }
+      else
+      {
+        c+=(fg-f);
+        f=fg+1;
+      }
+    }
+    
+    cout<<c<<" ";
+    sum--;
+  }
+  //cout<<c<<'\n';
+  
 }
 
 
