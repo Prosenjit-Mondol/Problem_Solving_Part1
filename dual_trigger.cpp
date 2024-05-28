2bcf7a568532d5762e9758d3fb0ff59425b4a493
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-05-29 00:14:48 +0600

    5/29/24

---
+++ dual_trigger.cpp
@@ -0,0 +1,63 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    set<char>st;
+    for(int i=0;i<n;i++)
+    {
+    st.insert(s[i]);
+    }
+    if (st.size()==1)
+    {
+        if (n==1)
+        {
+            cout<<"NO"<<'\n';
+        }
+        else
+        {
+            cout<<"YES"<<'\n';
+        }
+        
+    }
+    for (int i = 0; i < n-1; i++)
+    {
+        if (s[i]=='0'&&s[i+1]=='0')
+        {
+            cout<<"NO"<<'\n';
+            return;
+        }
+        
+    }
+    cout<<"YES"<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
