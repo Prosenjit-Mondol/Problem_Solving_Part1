affb361fd942f5b37dd49c2f15638a5a9c446b50
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-06-14 23:31:41 +0600

    6/14/24

--- line.cpp
+++ line.cpp
@@ -1,62 +1,30 @@
-
-//Author  :  PROSENJIT MONDOL
-
-
 #include<bits/stdc++.h>
-#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
-#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
-const int inf = 3e5;
-using ll=unsigned long long ;
-ll mod = 10e9 + 7;
+int  main()
+{
 
-//------------------------------------------------------------------------------
-
-void solve(void)
+int t;
+cin>>t;
+while(t--)
 {
     int n;
     string s;
     cin>>n>>s;
-    ll total=0;
-    vector<ll>v;
-    for (int i = 0; i < n; i++)
-    {
-        if (s[i]=='L')
-        {
-            v.push_back((n-i-1)-i);
-            total+=i;
-        }
-        else
-        {
-            v.push_back(i-(n-1-i));
-            total+=(n-1-i);
-        }
-          
-    }
-    sort(v.begin(),v.end(),greater<ll>());
-    for (int i = 0; i <n; i++)
-    {
-       if (v[i]>0)
-       {
-        total+=v[i];
-       }
-       cout<<total<<" ";
-    }
-    cout<<'\n';
-}
-
-
-//------------------------------------------------------------------------------
-int main()
+long long int tot=0;
+vector<long long>v(n);
+for(int i=0;i<n;i++)
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    if(s[i]=='L'){v.push_back((n-1-i)-i);tot+=i;}
+    else{v.push_back(i-(n-1-i));tot+=(n-1-i);}
+}
+sort(v.begin(),v.end(),greater<>());
+for(int i=0;i<n;i++)
+{
 
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
+    if(v[i]>0){tot+=v[i];}
+    cout<<tot<<' ';
+}
+cout<<endl;
+}
 return 0;
 }
