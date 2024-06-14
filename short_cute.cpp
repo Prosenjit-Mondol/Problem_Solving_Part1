8ca63a7047964aaa9bbcca268fa135402b3a60ba
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-06-14 22:11:44 +0600

    6/14/24

---
+++ short_cute.cpp
@@ -0,0 +1,51 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    ll sum=0;
+    for (ll i = 0; i <n; i++)
+    {
+        for (ll j= i+1; j< n; j++)
+        {
+            sum+=max((v[j]-v[i]),0LL);
+        }
+        
+    }
+    cout<<sum<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  //cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
