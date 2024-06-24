8f886f10f113ba7cbf29d0c8590161fc2ea89b6c
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-06-24 21:13:50 +0600

    6/24/24

---
+++ empty_string.cpp
@@ -0,0 +1,53 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    string s;
+    cin>>s;
+    int l=s.length();
+    int a=0;
+    for (int i = 0; i <l; i++)
+    {
+        if (s[i]=='a')
+        {
+            a++;
+        }
+        
+    }
+    if (a==l/2)
+    {
+        cout<<"YES"<<'\n';
+    }
+    else
+    {
+        cout<<"NO"<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
