7e9f6bdd2b6e82a67d486d6dfa6eeafbfdb026a4
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-06-24 21:48:24 +0600

    6/24/24

---
+++ gulshan_candy.cpp
@@ -0,0 +1,52 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n;
+    cin>>n;
+    map<int,int>mp;
+    for (int i = 0; i <n; i++)
+    {
+       int x;
+       cin>>x;
+       mp[x]++;
+    }
+    ll f=0,ans=0;
+    for(auto&x:mp)
+    {
+        if (ans<x.second)
+        {
+            ans=x.second;
+            f=x.first;
+        }
+    }
+    ans+=max(mp[f-1],mp[f+1]);
+    cout<<ans<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
