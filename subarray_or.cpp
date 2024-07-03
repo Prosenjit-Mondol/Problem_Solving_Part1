ba1e0005416dec4472ce122e4c8610c0e47635d8
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-07-03 17:08:26 +0600

    7/3/24

---
+++ subarray_or.cpp
@@ -0,0 +1,59 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n;
+    cin>>n;
+    map<ll,ll>mp;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+        mp[o]++;
+    }
+    ll c=n;
+    for (int i = 0; i <n; i++)
+    {
+        for (int j= i+1;j<n; j++)
+        {
+            ll x=v[i]|v[j];
+            if (mp[x]==0)
+            {
+                c++;
+                mp[x]=1;
+            }
+            
+        }
+        
+    }
+    cout<<c<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
