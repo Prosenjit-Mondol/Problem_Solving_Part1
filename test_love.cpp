9092a75e1fe5602fd5f321c56e715b98fbd689ae
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-07-28 21:51:45 +0600

    7/28/24

--- test_love.cpp
+++ test_love.cpp
@@ -1,33 +1,55 @@
-//Author  :  PROSENJIT MONDOL
+#include <iostream>
+#include <vector>
+#include <string>
 
-
-#include<bits/stdc++.h>
-#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
-#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
-const int inf = 3e5;
-using ll=long long ;
-ll mod = 1e9 + 7;
 
-//------------------------------------------------------------------------------
+void run() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    string A;
+    cin >> A;
+    
+    vector<int> logs;
+    for (int i = 0; i < n; ++i) {
+        if (A[i] == 'L') {
+            logs.push_back(i);
+        }
+    }
+    logs.push_back(n + 1);  // End marker
 
-void solve(void)
-{
+    int i = -1;
+    int next_log = 0;
 
+    while (i < n - 1) {
+        if (m >= logs[next_log] - i) {
+            i = logs[next_log];
+        } else {
+            i += m;
+            if (i > n - 1) {
+                cout << "YES" << endl;
+                return;
+            }
+            while (i < n && i < logs[next_log]) {
+                if (A[i] != 'C' && k > 0) {
+                    ++i;
+                    --k;
+                } else {
+                    cout << "NO" << endl;
+                    return;
+                }
+            }
+        }
+        ++next_log;
+    }
+    cout << "YES" << endl;
 }
 
-
-//------------------------------------------------------------------------------
-int main()
-{
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        run();
+    }
+    return 0;
 }
