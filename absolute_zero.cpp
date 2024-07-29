5f99a53430d8eb713ed6a9ac67349dc88e6d16cb
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-07-29 06:57:42 +0600

    7/29/24

--- absolute_zero.cpp
+++ absolute_zero.cpp
@@ -1,1 +1,63 @@
+//Author  :  PROSENJIT MONDOL
 
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    int n;
+    cin>>n;
+    vector<ll>v;
+    for(int i=0;i<n;i++)
+    {
+        ll o;
+        cin>>o;
+        v.push_back(o);
+    }
+    int ok=1;
+    for (int  i = 0; i < n; i++)
+    {
+        if (v[i]%2!=v[0]%2)
+        {
+            ok=0;
+            break;
+        }
+        
+    }
+    if (!ok)
+    {
+        cout<<-1<<'\n';
+        return;
+    }
+    sort(v.begin(),v.end());
+    cout<<32<<'\n'<<v[0];
+    for (int i =29; i>=0; i--)
+    {
+        cout<< " "<<(1<<i);
+    }
+    cout<<" "<<1<<'\n';
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
