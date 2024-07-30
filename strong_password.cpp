ca8dc1d454101213e2dd86e17ca7e11047df5637
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-07-30 21:16:50 +0600

    7/30/24

--- strong_password.cpp
+++ strong_password.cpp
@@ -1,12 +1,11 @@
-//Author  :  PROSENJIT MONDOL
+// Author  :  PROSENJIT MONDOL
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll=long long ;
+using ll = long long;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
@@ -14,27 +13,39 @@ ll mod = 1e9 + 7;
 void solve(void)
 {
     string s;
-    int c=0;
-    cin>>s;
-    if (s.length()==1)
+    int c = 0;
+    cin >> s;
+    if (s.length() == 1)
     {
-        if (s=="z")
+        if (s == "z")
         {
-            cout<<"az"<<'\n';
+            cout << "az" << '\n';
         }
         else
         {
-            char x=s[0]+1;
-            cout<<s<<x<<'\n';
+            char x = s[0] + 1;
+            cout << s << x << '\n';
         }
         return;
     }
-    for (int i = 0; i <s.length(); i++)
+
+
+
+
+
+    for (int i = 0; i < s.length(); i++)
     {
-        if (c==0&&s[i]==s[i+1])
+        if (c==0 && s[i]==s[i+1])
         {
-            char x=s[i]+1;
-            cout<<s[i]<<x;
+            if (s[i]=='z')
+            {
+                cout<<"za";
+            }
+            else
+            {
+                char x=s[i]+1;
+                cout<<s[i]<<x;
+            }
             c=1;
         }
         else
@@ -43,21 +54,22 @@ void solve(void)
         }
         
     }
-    cout<<'\n';
-}
+    cout << '\n';
 
+
+}
 
 //------------------------------------------------------------------------------
 int main()
 {
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-  ll t=1;
-  cin>>t;
-    while(t--)
-     {
-       solve();
-     }
-return 0;
+    ll t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
