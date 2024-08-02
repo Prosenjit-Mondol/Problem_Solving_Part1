5ca38972637e74abcf3c79eb11924c30ab84cb46
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-02 08:41:44 +0600

    8/2/24

--- leg.cpp
+++ leg.cpp
@@ -1,45 +1,45 @@
-// Author  :  PROSENJIT MONDOL
+//Author  :  PROSENJIT MONDOL
 
-#include <bits/stdc++.h>
+
+#include<bits/stdc++.h>
 #define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
 #define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
 using namespace std;
 const int inf = 3e5;
-using ll = long long;
+using ll=long long ;
 ll mod = 1e9 + 7;
 
 //------------------------------------------------------------------------------
 
 void solve(void)
 {
-  ll sum = 0, n;
-  string s;
-  cin >> n >> s;
-  stack<int> st;
-  sum=n/2;
-  for (int i = 0; i < n; i++)
-  {
-    if (s[i]=='(')
+    int n;
+    cin>>n;
+    int r=n%4;
+    if (r==2)
     {
-      sum+=2;
+        r=1+n/4;
+    }
+    else
+    {
+        r=n/4;
     }
     
-  }
-  
-  cout << sum << '\n';
+    cout<<r<<'\n';
 }
+
 
 //------------------------------------------------------------------------------
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
 
-  ll t = 1;
-  cin >> t;
-  while (t--)
-  {
-    solve();
-  }
-  return 0;
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
 }
