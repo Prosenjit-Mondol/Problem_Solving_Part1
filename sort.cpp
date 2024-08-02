a8075d57be31877980fc5490e588b49ea0030aa6
Prosenjit_Mondol <prosenjit1156@gmail.com>
2024-08-02 10:39:20 +0600

    8/2/24

---
+++ sort.cpp
@@ -0,0 +1,59 @@
+//Author  :  PROSENJIT MONDOL
+
+
+#include<bits/stdc++.h>
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+using namespace std;
+const int inf = 3e5;
+using ll=long long ;
+ll mod = 1e9 + 7;
+
+//------------------------------------------------------------------------------
+
+void solve(void)
+{
+    ll n,q;
+    string a,b;
+    cin>>n>>q>>a>>b;
+    for (int i = 0; i < q; i++)
+    {
+        ll l,u;
+        cin>>l>>u;
+        map<char,int>mp;
+        for (int j = l-1; j< u; j++)
+        {
+            mp[a[j]]++;
+        }
+        
+        for (int j = l-1; j< u; j++)
+        {
+            mp[b[j]]--;
+        }
+        
+        ll sum=0;
+        for(auto&x:mp)
+        {
+            if(x.second>0)
+            sum+=x.second;
+        }
+        cout<<sum<<'\n';
+    }
+    
+}
+
+
+//------------------------------------------------------------------------------
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+
+  ll t=1;
+  cin>>t;
+    while(t--)
+     {
+       solve();
+     }
+return 0;
+}
